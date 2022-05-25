b956176f9283bf492dfc31db48b78f44565941e4
Jean Carlos de Paula <jeandepaula294@gmail.com>
2022-05-25 16:02:55 -0300

    feat: Added routes to create an account and get the statement

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+const app = express()
+app.use(express.json())
+
+const customers = []
+
+app.post('/account', (req, res) => {
+    const { cpf, name } = req.body
+
+    const custumerAlreadyExists = customers.some(customer => customer.cpf === cpf)
+
+    if(custumerAlreadyExists){
+        return res.status(400).json({error: 'Customer already exists'})
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return res.status(201).send()
+})
+
+app.get('/statement/:cpf', (req, res) => {
+    const { cpf } = req.params
+
+    const customer = customers.find(customer => customer.cpf = cpf)
+
+    return res.json(customer.statement)
+})
+
+app.listen(3000)
