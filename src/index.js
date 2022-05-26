7f99ca6573176df18c8d15b71f756d17a2584c29
Jean Carlos de Paula <jeandepaula294@gmail.com>
2022-05-26 09:52:36 -0300

    feat: Add middleware to valide the customer before return the statement of one account

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ app.use(express.json())
 
 const customers = []
 
+const verifyIsExistsAccountCPF = (req, res, next) => {
+    const { cpf } = req.headers
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if(!customer){
+        return res.status(400).json({error: 'Customer not found'})
+    }
+
+    req.customer = customer
+
+    return next()
+}
+
 app.post('/account', (req, res) => {
     const { cpf, name } = req.body
 
@@ -24,10 +38,8 @@ app.post('/account', (req, res) => {
     return res.status(201).send()
 })
 
-app.get('/statement/:cpf', (req, res) => {
-    const { cpf } = req.params
-
-    const customer = customers.find(customer => customer.cpf = cpf)
+app.get('/statement', verifyIsExistsAccountCPF,(req, res) => {
+    const { customer } = req
 
     return res.json(customer.statement)
 })
